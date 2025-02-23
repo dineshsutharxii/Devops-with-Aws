17ba96bd7998ea8161ea4ee8ecb433d019444d7b
Dinesh <dineshsuthar@gmail.com>
2025-02-23 08:10:14 +0000

    Added more testcases

--- tests/test_devops.py
+++ tests/test_devops.py
@@ -3,4 +3,7 @@ import pytest
 def test_sum_of_two_numbers():
 	assert 2 + 3 == 5
 def test_multi_of_two_numbers():
-	assert 2 * 3 == 6
+    assert 2 * 3 == 6
+
+def test_div_of_two_number():
+    assert 2/2 == 1
